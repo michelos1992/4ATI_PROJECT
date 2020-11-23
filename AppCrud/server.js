b4619397a0ac91b22bbe2a3ac2e626c9135932f8
Misiu <michel.wojciech@gmail.com>
2020-11-23 16:55:49 +0100

    create file project

---
+++ AppCrud/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+var corsOption = {
+    origin: 'http://localhost:8081'
+};
+
+app.use(cors(corsOption));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.json({message: 'welcom to my app'});
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is runnig on port: ${PORT}.`);
+});
